70ebe0af6b8c9c1cf6f2ecb3fa360c9c12391edf
joao victor <joaovprudencio13@gmail.com>
2024-03-01 15:35:48 -0300

    porjeto spring boot

---
+++ src/main/java/com/joaovictordev/mack1/Mack1Application.java
@@ -0,0 +1,13 @@
+package com.joaovictordev.mack1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mack1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mack1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/joaovictordev/mack1/Mack1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaovictordev.mack1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Mack1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
